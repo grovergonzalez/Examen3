82f39c7001f4d19fc94f4bd1b33637549ab01691
grovergonzalez <grover.gonzalez@ucb.edu.bo>
2023-06-01 09:10:14 -0400

    Creando el primer test en cypress con el monto venta

---
+++ cypress/e2e/venta.cy.js
@@ -0,0 +1,9 @@
+describe("Ventas", () => {
+    it("", () => {
+      cy.visit("/");
+      cy.get("#monto-venta").type(4);
+    //   cy.get("#sumar-button").click();
+    //   cy.get("#resultado-div").should("contain", "9");
+    });
+  });
+  
